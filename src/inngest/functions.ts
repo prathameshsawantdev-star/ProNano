ca730462f1a74862c0b5020e8f23f6e1e13ea304
prathameshsawantdev-star <prathameshsawant.dev@gmail.com>
2026-03-09 20:42:54 +0530

    "Implemented simple  firecrawl url scraper to enhance the prompt with context"

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,9 @@
 import { google } from "@ai-sdk/google";
 import { inngest } from "./client";
 import { generateText } from "ai";
+import Firecrawl from "@mendable/firecrawl-js"
+
+const firecrawl = new Firecrawl({ apiKey: process.env.FIRECRAWL_API_KEY });
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
@@ -11,14 +14,35 @@ export const helloWorld = inngest.createFunction(
     }
 )
 
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 export const generateTextFn = inngest.createFunction(
     { id: "generate-text" },
     { event: "test/generate-text" },
-    async ({ step }) => {
+    async ({ event, step }) => {
+        const { prompt } = event.data as { prompt: string };
+
+        const urls = await step.run("extract-urls", async () => {
+            const matches = prompt.match(URL_REGEX);
+            return matches || [];
+        }) as string[];
+
+        const scrapedContent = await step.run("scrape-urls", async () => {
+            const results = await Promise.all(
+                urls.map(async (url) => {
+                    const result = await firecrawl.scrape(url, { formats: ['markdown'] })
+                    return result.markdown || "";
+                })
+            )
+
+            return results.filter().join("\n\n");
+        })
+
+        const finalPrompt = scrapedContent ? `Context:\n${scrapedContent}\n\nQuestion:\n${prompt}` : `Question:\n${prompt}`
         return await step.run("generate-text-gemini", async () => {
             return await generateText({
                 model: google('gemini-2.5-flash'),
-                prompt: `Provide response under 50 words, Prompt: Whats today's date?`,   
+                prompt: finalPrompt,   
             });
         })
     }
