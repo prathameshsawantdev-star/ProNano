40674e439d0ada0524c5f487bc2483687ce68180
prathameshsawantdev-star <prathameshsawant.dev@gmail.com>
2026-03-13 10:39:27 +0530

    sentry user actions logs added

--- src/app/api/demo/page.tsx
+++ src/app/api/demo/page.tsx
@@ -1,26 +1,33 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { useAuth } from '@clerk/nextjs';
 import React, { useState } from 'react'
+import * as Sentry from "@sentry/nextjs";
 
 export default function Page() {
   const [loading, setLoading] = useState(false);
+  const { userId } = useAuth();
 
   const handleBlocking = async() => {
     setLoading(true);
+    Sentry.logger.info("User initiated blocking request", { userId });
     await fetch('/api/demo/blocking', { method: 'POST' })
     setLoading(false);
   }
  
   const handleClientError = () => {
+    Sentry.logger.warn("User got a client error", { userId });
     throw new Error("Client error: Something went wrong in the browser!")
   }
 
   const handleApiError = () => {
+    Sentry.logger.error("User triggered API error", { userId });
     fetch('/api/demo/error', { method: 'POST' })
   }
 
   const handleInngestError = () => {
+    Sentry.logger.fatal("User triggered Inngest function error", { userId });
     fetch('/api/demo/inngest-error', { method: 'POST' })
   }
 
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -43,6 +43,11 @@ export const generateTextFn = inngest.createFunction(
             return await generateText({
                 model: google('gemini-2.5-flash'),
                 prompt: finalPrompt,   
+                 experimental_telemetry: {
+    isEnabled: true,
+    recordInputs: true,
+    recordOutputs: true,
+  },
             });
         })
     }
